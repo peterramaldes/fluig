50eed4a5bc9ea49c7dd57d855b32284783ce8111
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2020-08-20 19:41:17 -0300

    Initial commit

---
+++ src/main/java/com/ficticiusclean/fluig/FluigApplication.java
@@ -0,0 +1,11 @@
+package com.ficticiusclean.fluig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FluigApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ficticiusclean/fluig/FluigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ficticiusclean.fluig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
